4de19ed78cd8280bebf17669fa16c59768d6db8f
utkarshh0110 <utkarshh0110@gmail.com>
2021-02-28 17:29:39 +0530

    First commit

---
+++ client/src/actions/posts.js
@@ -0,0 +1,59 @@
+import * as api from "../api";
+import {
+  FETCH_ALL,
+  CREATE,
+  DELETE,
+  LIKE,
+  UPDATE,
+} from "../constants/actionTypes";
+//Action creator(These are functions that return actions)
+//Since dealing with async logic so we are using reduc thunk. These would help in creating async.
+export const getPosts = () => async (dispatch) => {
+  try {
+    const { data } = await api.fetchPosts();
+    const action = { type: FETCH_ALL, payload: data };
+    dispatch(action);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export const createPost = (post) => async (dispatch) => {
+  try {
+    const { data } = await api.createPost(post);
+    const action = { type: CREATE, payload: data };
+    dispatch(action);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const updatePost = (id, updatedPost) => async (dispatch) => {
+  try {
+    const { data } = await api.updatePost(id, updatedPost);
+    const action = { type: UPDATE, payload: data };
+    dispatch(action);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const deletePost = (id) => async (dispatch) => {
+  try {
+    await api.deletePost(id);
+    const action = { type: DELETE, payload: id };
+    dispatch(action);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const likePost = (id) => async (dispatch) => {
+  try {
+    const { data } = await api.likePost(id);
+    const action = { type: LIKE, payload: data };
+    dispatch(action);
+  } catch (error) {
+    console.log(error);
+  }
+};
